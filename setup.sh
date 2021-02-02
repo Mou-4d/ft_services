8fcb7664b4cf5ecb9d3d1c18a5416c0374f53640
Mouaad Lasritene <mlasrite@e2r9p2.1337.ma>
2021-02-02 19:39:36 +0100

    .

--- setup.sh
+++ setup.sh
@@ -4,9 +4,14 @@ minikube start --vm-driver=virtualbox
 
 eval $(minikube -p minikube docker-env)
 # kubectl delete deploy nginx-deployment
+
 #build all images
 docker build -t mynginx srcs/nginx/
 
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl apply -f srcs/configMap.yml
 
 #apply  yaml files for each deplyomenet
 kubectl apply -f srcs/nginx/nginx.yml
