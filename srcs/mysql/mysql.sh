e04f3b5121d27ccd9cda6aee0e57d1b568e89421
Mouaad Lasritene <mouadlas@Mouaads-MacBook-Pro.local>
2021-02-04 15:23:15 +0100

    .

--- srcs/mysql/mysql.sh
+++ srcs/mysql/mysql.sh
@@ -1,6 +1,11 @@
-mkdir -p /var/lib/mysql /run/mysqld/
+mkdir -p /var/lib/mysql /run/mysqld
 
-mysql_install_db -u root --datadir=/var/lib/mysql > /dev/null
+/usr/bin/mysql_install_db --datadir=/var/lib/mysql
 
+exec /usr/bin/mysqld -u root & sleep 5
+
+#create wp database and user
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'%';FLUSH PRIVILEGES;"
 
+#keep container running
+sleep infinity
