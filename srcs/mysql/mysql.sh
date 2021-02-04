8036f36209f6dd7a2e17a722b6e52e6d70da9529
Mouaad Lasritene <mlasrite@e2r9p2.1337.ma>
2021-02-04 13:00:06 +0100

    .

--- srcs/mysql/mysql.sh
+++ srcs/mysql/mysql.sh
@@ -4,4 +4,3 @@ mysql_install_db -u root --datadir=/var/lib/mysql > /dev/null
 
 mysql -u root -e "CREATE DATABASE wp_db;CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'%';FLUSH PRIVILEGES;"
 
-supervisord -c /etc/supervisord.conf
