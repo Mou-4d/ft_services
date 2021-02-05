13702275f806fd8da56f4de7fce77d5701da1c50
Mouaadz <mouadlas@Mouaads-MacBook-Pro.local>
2021-02-05 23:55:00 +0100

    .

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -26,10 +26,10 @@ define( 'DB_NAME', 'wp_db' );
 define( 'DB_USER', 'mouadz' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'mouadz' );
+define( 'DB_PASSWORD', 'dev' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql' );
+define( 'DB_HOST', '10.108.244.184' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
