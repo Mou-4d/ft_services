330684b3d249f820a4fefbd90bafbeb27f3500df
Mouaad Lasritene <mouadlas@Mouaads-MacBook-Pro.local>
2021-02-05 11:45:23 +0100

    .

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ eval $(minikube -p minikube docker-env)
 docker build -t nginx srcs/nginx/
 docker build -t mysql srcs/mysql/
 docker build -t wordpress srcs/wordpress/
+docker build -t phpmyadmin srcs/phpmyadmin/
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
@@ -18,4 +19,5 @@ kubectl apply -f srcs/configMap.yml
 kubectl apply -f srcs/nginx/nginx.yml
 kubectl apply -f srcs/mysql/mysql.yml
 kubectl apply -f srcs/wordpress/wordpress.yml
+kubectl apply -f srcs/phpmyadmin/phpmyadmin.yml
 
