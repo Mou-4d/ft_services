19f1f4df0bf8f6f12ad19a6e4f88d6fae5fff411
Mouadz <mlasrite@student.1337.ma>
2021-02-06 15:40:32 +0100

    .

--- srcs/phpmyadmin/phpmyadmin.sh
+++ srcs/phpmyadmin/phpmyadmin.sh
@@ -4,6 +4,7 @@ wget http://files.directadmin.com/services/all/phpMyAdmin/phpMyAdmin-5.0.2-all-l
 tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
 rm phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages /var/www/localhost/htdocs/phpmyadmin
+mv config.inc.php /var/www/localhost/htdocs/phpmyadmin/
 
 
 rc-status
