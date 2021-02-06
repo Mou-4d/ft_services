22c8bbfdae737b168583207c9cebf0f859c48a41
Mouaadz <mouadlas@Mouaads-MacBook-Pro.local>
2021-02-06 12:18:01 +0100

    .

--- srcs/wordpress/wordpress.sh
+++ srcs/wordpress/wordpress.sh
@@ -1,21 +1,16 @@
 
 mkdir run/nginx
 
-tar -xvf wordpress.tar.gz
+wget http://wordpress.org/latest.tar.gz
+tar -xvf latest.tar.gz
+rm latest.tar.gz
 cp -r wordpress /var/www/localhost/htdocs/
-rm wordpress.tar.gz
-
+rm -rf wordpress
 mv wp-config.php /var/www/localhost/htdocs/wordpress/
 
-#Creating new user and group 'www' for nginx
-adduser -D -g 'www' www
-
-#Create a directory for html files
-
-chown -R www:www /var/www/localhost/htdocs/wordpress
-chown -R www:www /var/lib/nginx
 
 rc-status
 touch /run/openrc/softlevel
 rc-service php-fpm7 start
 nginx -g 'daemon off;'
+
