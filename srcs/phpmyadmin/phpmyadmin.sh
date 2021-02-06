0155afa806aa089977e704ce22b5a1d61f96f979
Mouadz <mlasrite@student.1337.ma>
2021-02-06 12:46:33 +0100

    .

--- srcs/phpmyadmin/phpmyadmin.sh
+++ srcs/phpmyadmin/phpmyadmin.sh
@@ -1,1 +1,8 @@
-sleep infinity
+mkdir run/nginx
+
+wget http://files.directadmin.com/services/all/phpMyAdmin/phpMyAdmin-5.0.2-all-languages.tar.gz
+tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
+rm phpMyAdmin-5.0.2-all-languages.tar.gz
+
+
+nginx -g 'daemon off;'
