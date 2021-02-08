feafb3d14242cd569c8142c10ca322ef69d5d2b4
Mouadz <mlasrite@student.1337.ma>
2021-02-08 15:35:23 +0100

    .

--- srcs/grafana/grafana.sh
+++ srcs/grafana/grafana.sh
@@ -1,4 +1,9 @@
+
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise-7.4.0.linux-amd64.tar.gz
-tar -zxvf grafana-enterprise-7.4.0.linux-amd64.tar.gz
+
+tar -xvf grafana-enterprise-7.4.0.linux-amd64.tar
+rm grafana-enterprise-7.4.0.linux-amd64.tar
+
+cd /grafana-7.4.0/bin ; ./grafana-server
 
 sleep infinity
