d319a96f56787987926fb313630b62d1165ce6ae
Mouadz <mlasrite@student.1337.ma>
2021-02-08 18:29:20 +0100

    .

--- srcs/grafana/grafana.sh
+++ srcs/grafana/grafana.sh
@@ -4,9 +4,12 @@ wget https://dl.grafana.com/enterprise/release/grafana-enterprise-7.4.0.linux-am
 tar -xvf grafana-enterprise-7.4.0.linux-amd64.tar.gz
 rm grafana-enterprise-7.4.0.linux-amd64.tar.gz
 
+mkdir /grafana-7.4.0/data
+mv grafana.db /grafana-7.4.0/data/
 
 rc-status
 touch /run/openrc/softlevel
 rc-service telegraf start
+
 
 cd /grafana-7.4.0/bin ; ./grafana-server
