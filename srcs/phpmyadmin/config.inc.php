ffff589d87560ad32b547b0b97f43dfe12647084
Mouadz <mlasrite@student.1337.ma>
2021-02-09 11:30:53 +0100

    .

--- srcs/phpmyadmin/config.inc.php
+++ srcs/phpmyadmin/config.inc.php
@@ -74,6 +74,7 @@ $cfg['Servers'][$i]['AllowNoPassword'] = false;
  */
 $cfg['UploadDir'] = '';
 $cfg['SaveDir'] = '';
+$cfg['TempDir'] = '/tmp';
 
 /**
  * Whether to display icons or text or both icons and text in table row
--- srcs/phpmyadmin/phpmyadmin.sh
+++ srcs/phpmyadmin/phpmyadmin.sh
@@ -5,6 +5,8 @@ wget http://files.directadmin.com/services/all/phpMyAdmin/phpMyAdmin-5.0.2-all-l
 tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
 rm phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages /var/www/localhost/htdocs/phpmyadmin
+mkdir /var/www/localhost/htdocs/phpmyadmin/tmp
+chmod 777 /var/www/localhost/htdocs/phpmyadmin/tmp
 mv config.inc.php /var/www/localhost/htdocs/phpmyadmin/
 
 
