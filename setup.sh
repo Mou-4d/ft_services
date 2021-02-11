e57bd4b5a20dcf3b55d920df85595070278d1c88
Mouadz <mlasrite@student.1337.ma>
2021-02-11 19:19:16 +0100

    .

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ docker build -t influxdb srcs/influxdb/
 docker build -t grafana srcs/grafana/
 docker build -t ftps srcs/ftps/
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/configMap.yml
 
